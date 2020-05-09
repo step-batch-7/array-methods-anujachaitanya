033d563c18fc1331edf8373ad4dfcc359d31dd5f
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-05-09 12:57:26 +0530

    added test for reduce

--- test/test_main.c
+++ test/test_main.c
@@ -43,10 +43,19 @@ void test_reduce_for_empty_array(void)
 {
   char description[] = "Should return init as it is for empty list for reduce";
   int input_values[0];
-  Array_ptr expected = init_array(input_values, 0);
   Array_ptr actual = init_array(input_values, 0);
   int reduce_result = reduce(actual, 10, add);
   Bool result = assert_int(reduce_result, 10);
+  show_result(result, description);
+}
+
+void test_reduce_for_addition(void)
+{
+  char description[] = "Should return the value for list for add function for reduce";
+  int input_values[5] = {1, 2, 3, 4, 5};
+  Array_ptr actual = init_array(input_values, 5);
+  int reduce_result = reduce(actual, 0, add);
+  Bool result = assert_int(reduce_result, 15);
   show_result(result, description);
 }
 
@@ -55,5 +64,6 @@ int main(void)
   test_map_for_empty_array();
   test_map_for_any_mapper();
   test_reduce_for_empty_array();
+  test_reduce_for_addition();
   return 0;
 }
